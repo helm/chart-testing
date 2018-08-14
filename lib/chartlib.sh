cbe1869db57079221cdd11be7c8b2a9310e03f12
Reinhard Nägele <unguiculus@gmail.com>
2018-08-14 23:16:34 +0200

    WIP: Double-check pod readiness for deployments

--- lib/chartlib.sh
+++ lib/chartlib.sh
@@ -289,10 +289,29 @@ chartlib::install_chart_with_single_config() {
             helm install "$chart_dir" --name "$release" --namespace "$namespace" --wait --timeout "$TIMEOUT"
         fi
 
+        local error=
+
         # For deployments --wait may not be sufficient because it looks at 'maxUnavailable' which is 0 by default.
         for deployment in $(kubectl get deployment --namespace "$namespace" --output jsonpath='{.items[*].metadata.name}'); do
             kubectl rollout status "deployment/$deployment" --namespace "$namespace"
+
+            # 'kubectl rollout status' does not return a non-zero exit code when rollouts fail.
+            # We, thus, need to double-check here.
+
+            local jsonpath='{.status.conditions[?(@.type=="Ready")].status}'
+
+            for pod in $(chartlib::get_pods_for_deployment "$deployment" "$namespace"); do
+                ready=$(kubectl get pod "$pod" --namespace "$namespace" --output jsonpath="$jsonpath")
+                if [[ "$ready" != "True" ]]; then
+                    chartlib::error "Pod '$pod' did not reach ready state!"
+                    error=true
+                fi
+            done
         done
+
+        if [[ -n "$error" ]]; then
+            return 1
+        fi
 
         echo "Testing chart '$chart_dir' in namespace '$namespace'..."
         helm test "$release" --cleanup --timeout "$TIMEOUT"
@@ -302,6 +321,21 @@ chartlib::install_chart_with_single_config() {
         chartlib::error "Chart installation failed: $chart_dir"
         return 1
     fi
+}
+
+# Returns the pods that are governed by a deployment.
+# Args:
+#   $1 The name of the deployment
+#   $2 The namespace
+chartlib::get_pods_for_deployment() {
+    local deployment="${1?Deployment is required}"
+    local namespace="${2?Namespace is required}"
+
+    local jq_filter='.spec.selector.matchLabels | to_entries | .[] | "--selector=\(.key)=\(.value) "'
+    local selectors
+
+    mapfile -t selectors < <(kubectl get deployment "$deployment" --namespace "$namespace" --output=json | jq -r "$jq_filter")
+    kubectl get pods "${selectors[@]}" --namespace "$namespace" --output jsonpath='{.items[*].metadata.name}'
 }
 
 # Lints a chart for all custom values files matching '*.values.yaml'
