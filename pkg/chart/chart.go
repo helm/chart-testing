helm/chart-testing

commit 7b167f8fe019c521b8946b76dcbfbe9becf2a029

Fix index out of range when files exist in the root of a chart dir (#73)
Signed-off-by: Adam Kunicki <adam@streamsets.com>

pkg/chart/chart.go
@@ -408,10 +408,10 @@ func (t *Testing) ComputeChangedChartDirectories() ([]string, error) {
 	var changedChartDirs []string
 	for _, file := range allChangedChartFiles {
 		pathElements := strings.SplitN(filepath.ToSlash(file), "/", 3)
-		if util.StringSliceContains(cfg.ExcludedCharts, pathElements[1]) {
+		if len(pathElements) < 2 || util.StringSliceContains(cfg.ExcludedCharts, pathElements[1]) {
 			continue
 		}
-		dir := path.Join(pathElements[0], pathElements[1])
+		dir := filepath.Dir(file)
 		// Only add if not already in list and double-check if it is a chart directory
 		if !util.StringSliceContains(changedChartDirs, dir) && t.chartUtils.IsChartDir(dir) {
 			changedChartDirs = append(changedChartDirs, dir)

pkg/chart/chart_test.go
@@ -42,6 +42,7 @@ func (g fakeGit) MergeBase(commit1 string, commit2 string) (string, error) {
 
 func (g fakeGit) ListChangedFilesInDirs(commit string, dirs ...string) ([]string, error) {
 	return []string{
+		"fileAtRoot",
 		"incubator/excluded/Chart.yaml",
 		"incubator/excluded/values.yaml",
 		"incubator/bar/README.md",

commit 374227938c10fdd16c88d03a33d9209a7465052e

Require charts to be in direct subdirectories of configured charts dirs (#90)
Fixes a regression introduced in #73 so that chart directories were no longer required to be in a direct subdirectory of configured chart directories.

This caused problems in https://github.com/helm/charts/pull/10830.

cc @munnerz

pkg/chart/chart.go
@@ -457,7 +457,7 @@ func (t *Testing) ReadAllChartDirectories() ([]string, error) {
 	for _, chartParentDir := range cfg.ChartDirs {
 		dirs, err := t.directoryLister.ListChildDirs(chartParentDir,
 			func(dir string) bool {
-				_, err := t.chartUtils.LookupChartDir(chartDirs, dir)
+				_, err := t.chartUtils.LookupChartDir(cfg.ChartDirs, dir)
 				return err == nil && !util.StringSliceContains(cfg.ExcludedCharts, path.Base(dir))
 			})
 		if err != nil {

pkg/util/util.go
@@ -126,13 +126,19 @@ func (u ChartUtils) LookupChartDir(chartDirs []string, dir string) (string, erro
 	for _, chartDir := range chartDirs {
 		currentDir := dir
 		for {
-			if FileExists(path.Join(currentDir, "Chart.yaml")) {
+			chartYaml := path.Join(currentDir, "Chart.yaml")
+			parent := path.Dir(path.Dir(chartYaml))
+
+			// check directory has a Chart.yaml and that it is in a
+			// direct subdirectory of a configured charts directory
+			if FileExists(chartYaml) && (parent == chartDir) {
 				return currentDir, nil
 			}
-			currentDir = filepath.Dir(currentDir)
+
+			currentDir = path.Dir(currentDir)
 			relativeDir, _ := filepath.Rel(chartDir, currentDir)
-			joined := filepath.Join(chartDir, relativeDir)
-			if joined == chartDir {
+			joined := path.Join(chartDir, relativeDir)
+			if (joined == chartDir) || strings.HasPrefix(relativeDir, "..") {
 				break
 			}
 		}

pkg/chart/chart_test.go
@@ -42,50 +42,17 @@ func (g fakeGit) MergeBase(commit1 string, commit2 string) (string, error) {
 
 func (g fakeGit) ListChangedFilesInDirs(commit string, dirs ...string) ([]string, error) {
 	return []string{
-		"fileAtRoot",
-		"incubator/excluded/Chart.yaml",
-		"incubator/excluded/values.yaml",
-		"incubator/bar/README.md",
-		"incubator/bar/README.md",
-		"incubator/excluded/templates/configmap.yaml",
-		"incubator/excluded/values.yaml",
-		"stable/blah/Chart.yaml",
-		"stable/blah/README.md",
-		"foo/this-is-no-chart-dir/foo.md",
+		"test_charts/foo/Chart.yaml",
+		"test_charts/bar/Chart.yaml",
+		"test_charts/bar/bar_sub/templates/bar_sub.yaml",
+		"test_chart_at_root/templates/foo.yaml",
+		"some_non_chart_dir/some_non_chart_file",
+		"some_non_chart_file",
 	}, nil
 }
 
 func (g fakeGit) GetUrlForRemote(remote string) (string, error) {
 	return "git@github.com/helm/chart-testing", nil
-}
-
-type fakeDirLister struct{}
-
-func (l fakeDirLister) ListChildDirs(parentDir string, test func(dir string) bool) ([]string, error) {
-	if parentDir == "stable" {
-		var dirs []string
-		for _, dir := range []string{"stable/foo", "stable/excluded"} {
-			if test(dir) {
-				dirs = append(dirs, dir)
-			}
-		}
-		return dirs, nil
-	}
-	return []string{"incubator/bar"}, nil
-}
-
-type fakeChartUtils struct{}
-
-func (v fakeChartUtils) LookupChartDir(chartDirs []string, dir string) (string, error) {
-	if strings.HasPrefix(dir, "foo")  {
-		return "", errors.New("no chart dir")
-	}
-	return dir, nil
-}
-
-func (v fakeChartUtils) ReadChartYaml(dir string) (*util.ChartYaml, error) {
-	chartUtils := util.ChartUtils{}
-	return chartUtils.ReadChartYaml(dir)
 }
 
 type fakeAccountValidator struct{}
@@ -129,16 +96,16 @@ var ct Testing
 func init() {
 	cfg := config.Configuration{
 		ExcludedCharts: []string{"excluded"},
-		ChartDirs:      []string{"stable", "incubator"},
+		ChartDirs:      []string{"test_charts", "."},
 	}
 
 	fakeMockLinter := new(fakeLinter)
 
 	ct = Testing{
 		config:           cfg,
-		directoryLister:  fakeDirLister{},
+		directoryLister:  util.DirectoryLister{},
 		git:              fakeGit{},
-		chartUtils:       fakeChartUtils{},
+		chartUtils:       util.ChartUtils{},
 		accountValidator: fakeAccountValidator{},
 		linter:           fakeMockLinter,
 		helm:             fakeHelm{},
@@ -147,16 +114,22 @@ func init() {
 
 func TestComputeChangedChartDirectories(t *testing.T) {
 	actual, err := ct.ComputeChangedChartDirectories()
-	expected := []string{"incubator/bar", "stable/blah"}
+	expected := []string{"test_charts/foo", "test_charts/bar", "test_chart_at_root"}
+	for _, chart := range actual {
+		assert.Contains(t, expected, chart)
+	}
+	assert.Len(t, actual, 3)
 	assert.Nil(t, err)
-	assert.Equal(t, actual, expected)
 }
 
 func TestReadAllChartDirectories(t *testing.T) {
 	actual, err := ct.ReadAllChartDirectories()
-	expected := []string{"stable/foo", "incubator/bar"}
+	expected := []string{"test_charts/foo", "test_charts/bar", "test_chart_at_root"}
+	for _, chart := range actual {
+		assert.Contains(t, expected, chart)
+	}
+	assert.Len(t, actual, 3)
 	assert.Nil(t, err)
-	assert.Equal(t, actual, expected)
 }
 
 func TestValidateMaintainers(t *testing.T) {