becae06b9aa0d5824fbb3653cbab22a12376ddff
rimas <rmocius@gmail.com>
2019-02-21 12:42:38 +0000

    Update kind cluster example with working local storage for all nodes
    Signed-off-by: rimas <rmocius@gmail.com>

--- examples/kind/test/e2e-kind.sh
+++ examples/kind/test/e2e-kind.sh
@@ -74,18 +74,15 @@ install_tiller() {
     echo
 }
 
-install_hostpath-provisioner() {
-    # kind doesn't support Dynamic PVC provisioning yet, this is one way to get it working
-    # https://github.com/rimusz/charts/tree/master/stable/hostpath-provisioner
+install_local-path-provisioner() {
+    # kind doesn't support Dynamic PVC provisioning yet, this is one ways to get it working
+    # https://github.com/rancher/local-path-provisioner
 
-    echo 'Installing hostpath-provisioner...'
-
-    # Remove default storage class. Will be recreated by hostpath -provisioner
+    # Remove default storage class. It will be recreated by local-path-provisioner
     docker_exec kubectl delete storageclass standard
 
-    docker_exec helm repo add rimusz https://charts.rimusz.net
-    docker_exec helm repo update
-    docker_exec helm install rimusz/hostpath-provisioner --name hostpath-provisioner --namespace kube-system --wait
+    echo 'Installing local-path-provisioner...'
+    docker_exec kubectl apply -f test/local-path-provisioner.yaml
     echo
 }
 
@@ -100,7 +97,7 @@ main() {
 
     create_kind_cluster
     install_tiller
-    install_hostpath-provisioner
+    install_local-path-provisioner
     install_charts
 }
 
