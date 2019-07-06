bc964d19c1b0dee37aa0fcab2d07442ba2021eef
Reinhard Naegele <unguiculus@gmail.com>
2019-07-06 19:16:57 +0200

    Consider username and password in repo urls
    Signed-off-by: Reinhard Naegele <unguiculus@gmail.com>

--- pkg/tool/account_test.go
+++ pkg/tool/account_test.go
@@ -16,10 +16,13 @@ func TestParseOutGitDomain(t *testing.T) {
 	}{
 		{"GitHub SSH", "git@github.com:foo/bar", "github.com", nil},
 		{"GitHub HTTPS", "https://github.com/foo/bar", "github.com", nil},
+		{"GitHub HTTPS with username/password", "https://foo:token@github.com/foo/bar", "github.com", nil},
 		{"Gitlab SSH", "git@gitlab.com:foo/bar", "gitlab.com", nil},
 		{"Gitlab HTTPS", "https://gitlab.com/foo/bar", "gitlab.com", nil},
+		{"Gitlab HTTPS with username/password", "https://gitlab-ci-token:password@gitlab.com/foo/bar", "gitlab.com", nil},
 		{"Bitbucket SSH", "git@bitbucket.com:foo/bar", "bitbucket.com", nil},
 		{"Bitbucket HTTPS", "https://bitbucket.com/foo/bar", "bitbucket.com", nil},
+		{"Bitbucket HTTPS with username/password", "https://user:pass@bitbucket.com/foo/bar", "bitbucket.com", nil},
 		{"Invalid", "foo/bar", "", fmt.Errorf("Could not parse git repository domain for 'foo/bar'")},
 	}
 
