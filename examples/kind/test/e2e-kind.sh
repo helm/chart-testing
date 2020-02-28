00d8fd274ac43d1020f5a57e11c2b9028aa854dd
Reinhard Nägele <unguiculus@gmail.com>
2020-02-28 17:56:13 +0100

    Prepare release v3.0.0-beta.2 (#208)
    Signed-off-by: Reinhard Naegele <unguiculus@gmail.com>

--- examples/kind/test/e2e-kind.sh
+++ examples/kind/test/e2e-kind.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly CT_VERSION=v3.0.0-beta.1
+readonly CT_VERSION=v3.0.0-beta.2
 readonly KIND_VERSION=v0.7.0
 readonly CLUSTER_NAME=chart-testing
 readonly K8S_VERSION=v1.17.0
