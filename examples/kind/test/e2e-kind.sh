b5f6ea2f92782337eead121939241f5a3a462aeb
Reinhard Nägele <unguiculus@gmail.com>
2021-05-20 17:18:33 +0200

    Update docs and examples for v3.4.0
    Signed-off-by: Reinhard Nägele <unguiculus@gmail.com>

--- examples/kind/test/e2e-kind.sh
+++ examples/kind/test/e2e-kind.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly CT_VERSION=v3.1.1
+readonly CT_VERSION=v3.4.0
 readonly KIND_VERSION=v0.7.0
 readonly CLUSTER_NAME=chart-testing
 readonly K8S_VERSION=v1.17.0
