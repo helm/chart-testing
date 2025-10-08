eb5edeb946c39363d1809407462c801a542fc1af
Adrian Berger <43774417+adberger@users.noreply.github.com>
2025-10-08 09:27:08 +0200

    Prepare release v3.14.0 (#778)
    Signed-off-by: Adrian Berger <adrian.berger@bedag.ch>

--- examples/kind/test/e2e-kind.sh
+++ examples/kind/test/e2e-kind.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly CT_VERSION=v3.7.1
+readonly CT_VERSION=v3.14.0
 readonly KIND_VERSION=v0.12.0
 readonly CLUSTER_NAME=chart-testing
 readonly K8S_VERSION=v1.22.7
